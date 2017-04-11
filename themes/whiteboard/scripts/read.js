51813b058cef3565cb6d3686d311c66a88e2d9c7
Filip Szczechowiak <filip.szczechowiak@gmail.com>
2017-04-11 12:09:08 +0200

    fix an issue with includes from subdirectories

--- themes/whiteboard/scripts/read.js
+++ themes/whiteboard/scripts/read.js
@@ -4,8 +4,7 @@ var fs = require('fs');
 var pathFn = require('path');
 
 hexo.extend.helper.register('read', function(path){
-
-  var src = pathFn.join(hexo.source_dir, 'includes', path);
+  var src = pathFn.join(hexo.source_dir, 'includes', pathFn.join(pathFn.dirname(file), '_' + pathFn.basename(file) + '.md'));
   if( fs.existsSync(src) ) {
     return fs.readFileSync(src, {
       encoding: 'utf-8'
