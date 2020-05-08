e4ba03844123ca67909182433d4f0509f043bd6c
Deisss <villette.charles@gmail.com>
2020-05-09 01:28:06 +0200

    Dummy changes

--- themes/whiteboard/scripts/read.js
+++ themes/whiteboard/scripts/read.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
+const fs = require("fs");
+const pathFn = require("path");
 
-var fs = require('fs');
-var pathFn = require('path');
-
-hexo.extend.helper.register('read', function(path){
-  var src = pathFn.join(hexo.source_dir, 'includes', pathFn.join(pathFn.dirname(path), '_' + pathFn.basename(path) + '.md'));
-  if( fs.existsSync(src) ) {
-    return fs.readFileSync(src, {
-      encoding: 'utf-8'
-    });
-  }
-  return '';
+hexo.extend.helper.register("read", function(path) {
+    const src = pathFn.join(hexo.source_dir, "includes", pathFn.join(pathFn.dirname(path), "_" + pathFn.basename(path) + ".md"));
+    if (fs.existsSync(src)) {
+        return fs.readFileSync(src, {
+            encoding: "utf-8"
+        });
+    }
+    return "";
 });
